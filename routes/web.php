3fabbc05e00e0cbdce36babe65d8034c5c958c1c
JulianoSilva05 <julianoqm@gmail.com>
2024-05-12 22:49:12 -0300

    Introdução ao laravel - MVC

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('home');
 });
+*/
+//há duas formas de chamar uma view. A primeira é por função, a segunda chamando direto a view
+Route::view('/', 'home');
+
+Route::get('/clientes', function () {
+    echo "Pagina com os clientes cadastrados";
+});
+
+Route::get('/clientes/{codigo}', function ($codigo) {
+    echo "Essa é a pagina do cliente ".$codigo ;
+});
+
+Route::get('/clientes/{codigo}/nome/{nome}', function ($codigo,$nome) {
+    echo "Essa é a pagina do cliente ".$codigo. " - " .$nome;
+});
+
+
